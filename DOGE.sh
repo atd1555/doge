5fa4a72d110953ec69fff12345825a4cd14f5fa7
atd1555 <82619294+atd1555@users.noreply.github.com>
2021-04-15 19:04:55 -0400

    Update DOGE.sh

--- DOGE.sh
+++ DOGE.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./t-rex.exe -o kp.unmineable.com:3333 -a kawpow -u DOGE:YOURWALLET.myminer#cw0i-ege7 -p x
+./t-rex.exe -o kp.unmineable.com:3333 -a kawpow -u DOGE:DPM6arXuB6U38kcdTxT9GFnQ7PS8j4osCb.myminer#cw0i-ege7 -p x
